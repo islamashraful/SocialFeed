7f3717d1c43aeffbca6408ddd48e91792ba2e373
Ashraful Islam <rx.ju.edu@gmail.com>
2023-11-06 22:10:24 +0700

    Implement infinite scroll for user posts

--- App.tsx
+++ App.tsx
@@ -128,27 +128,41 @@ const USER_POSTS: Post[] = [
 ];
 
 const USER_STORIES_PAGE_SIZE = 4;
+const USER_POSTS_PAGE_SIZE = 2;
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [paginatedData, setPaginatedData] = useState<Story[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const pagination = (database: Story[], page: number) => {
-    const startIndex = (page - 1) * USER_STORIES_PAGE_SIZE;
-    const endIndex = startIndex + USER_STORIES_PAGE_SIZE;
+  const [currentPostsPage, setCurrentPostsPage] = useState(1);
+  const [paginatedPostsData, setPaginatedPostsData] = useState<Post[]>([]);
+  const [isLoadingPosts, setIsLoadingPosts] = useState(false);
+
+  function pagination<T>(database: T[], page: number, size: number): T[] {
+    const startIndex = (page - 1) * size;
+    const endIndex = startIndex + size;
     if (startIndex >= database.length) {
       return [];
     }
 
     return database.slice(startIndex, endIndex);
-  };
+  }
 
   useEffect(() => {
     setIsLoading(true);
-    const data = pagination(USER_STORIES, currentPage);
+    const data = pagination(USER_STORIES, currentPage, USER_STORIES_PAGE_SIZE);
     setPaginatedData(data);
     setIsLoading(false);
+
+    setIsLoadingPosts(true);
+    const posts = pagination(
+      USER_POSTS,
+      currentPostsPage,
+      USER_POSTS_PAGE_SIZE,
+    );
+    setPaginatedPostsData(posts);
+    setIsLoadingPosts(false);
   }, []);
 
   return (
@@ -181,6 +195,7 @@ const App = () => {
                     const contentsToAppend = pagination(
                       USER_STORIES,
                       currentPage + 1,
+                      USER_STORIES_PAGE_SIZE,
                     );
                     if (contentsToAppend.length) {
                       setCurrentPage(currentPage + 1);
@@ -202,7 +217,24 @@ const App = () => {
               </View>
             </>
           }
-          data={USER_POSTS}
+          onEndReachedThreshold={0.5}
+          onEndReached={() => {
+            if (isLoadingPosts) {
+              return;
+            }
+            setIsLoadingPosts(true);
+            const contentsToAppend = pagination(
+              USER_POSTS,
+              currentPostsPage + 1,
+              USER_POSTS_PAGE_SIZE,
+            );
+            if (contentsToAppend.length) {
+              setCurrentPostsPage(currentPostsPage + 1);
+              setPaginatedPostsData(prev => [...prev, ...contentsToAppend]);
+            }
+            setIsLoadingPosts(false);
+          }}
+          data={paginatedPostsData}
           showsVerticalScrollIndicator={false}
           renderItem={({item}) => <UserPost {...item} />}
           keyExtractor={item => item.id.toString()}
